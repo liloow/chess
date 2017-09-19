ca083672b4f044ca9a5f0265129709c3f0d44f1a
Thomas <tom.beguin@gmail.com>
2017-09-19 11:22:54 +0200

    pawns of both color moving

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,28 @@
 function Board() {
   this.grid = new Array(1).fill(null).map(function(item, row) {
     return new Array(4).fill(null).map(function(item, col) {
-      return new Pawn(row, col)
-    })
-  })
+      return new Pawn(row, col, 'white')
+    });
+  });
   this.grid.push([null, null, null, null])
   this.grid.push([null, null, null, null])
-  this.grid.push([null, null, null, null])
+  this.grid.push((new Array(1).fill(null).map(function(item, row) {
+    return new Array(4).fill(null).map(function(item, col) {
+      return new Pawn(row+3, col, 'black')
+    });
+  }))[0])
 }
 
-
-function Pawn(row, col) { // can only move forward : 1 case (TO DO: 2 case if firstmove)
+function Pawn(row, col, color) { // can only move forward : 1 case (TO DO: 2 case if firstmove)
   this.class = 'pawn'
+  this.color = color
   this.pos = {
     x: col,
     y: row,
   }
   this.moved = false
-  this.legalMoves = [this.pos.x,this.pos.y+1]
+  this.legalMoves = []
+  this.range = 1
 }
 
 Board.prototype.render = function() {
@@ -26,31 +31,33 @@ Board.prototype.render = function() {
   })
 }
 
-
-
-
-Board.prototype.updateboard = function(lastplayed) {
-
-}
-
 Pawn.prototype.move = function(x, y) {
   console.log(this)
-  console.log(this._canMove(x,y))
-  if (this._canMove(x,y)) {
-    var temp = board.grid[0][2]
+  console.log(this._canMove(x, y))
+  if (this._canMove(x, y)) {
+    var temp = board.grid[this.pos.y][this.pos.x]
     board.grid[y][x] = temp
-    board.grid[0].splice(2,1,null)
+    board.grid[this.pos.y].splice(this.pos.x, 1, null)
+    this.pos.y = y
+    this.pos.x = x
     board.render()
   }
 
 }
 
 
-
 Pawn.prototype._canMove = function(x, y) {
-  this.legalMoves = [this.pos.x, this.pos.y + 1]
-  if (this.legalMoves[0] === x && this.legalMoves[1] === y) {
-    return true
+  if (this.color === 'white') {
+    this.legalMoves = [this.pos.x, this.pos.y + 1]
+    if (this.legalMoves[0] === x && this.legalMoves[1] === y) {
+      return true
+    }
+  }
+  else {
+    this.legalMoves = [this.pos.x, this.pos.y - 1]
+    if (this.legalMoves[0] === x && this.legalMoves[1] === y) {
+      return true
+    }
   }
   return false
 }
