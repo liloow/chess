ebb9ca32954d0aed876c8d7d603cea3207419718
Thomas <tom.beguin@gmail.com>
2017-09-20 11:52:02 +0200

    Queen added and few optimizations

--- board.js
+++ board.js
@@ -13,7 +13,7 @@ function Board() {
       return new Pawn(row + 3, col, 'black')
     });
   }))[0])
-    this.grid.push((new Array(1).fill(null).map(function(item, row) {
+  this.grid.push((new Array(1).fill(null).map(function(item, row) {
     return new Array(8).fill(null).map(function(item, col) {
       return new Pawn(row + 3, col, 'black')
     });
@@ -32,6 +32,8 @@ function Board() {
   this.grid[7].splice(2, 1, new Bishop(7, 2, 'black'))
   this.grid[0].splice(5, 1, new Bishop(0, 5, 'white'))
   this.grid[7].splice(5, 1, new Bishop(7, 5, 'black'))
+  this.grid[0].splice(3, 1, new Queen(0, 3, 'white'))
+  this.grid[7].splice(3, 1, new Queen(7, 3, 'black'))
 }
 
 Board.prototype.render = function() {
@@ -41,7 +43,7 @@ Board.prototype.render = function() {
 }
 
 var PiecePrototype = {
-  checkLegal: function(x,y) {
+  checkLegal: function(x, y) {
     var legal = this.legalMoves.some(function(moveset) {
       return moveset[0] === x && moveset[1] === y
     })
@@ -50,7 +52,7 @@ var PiecePrototype = {
     }
     return false
   },
-    move: function(x, y) {
+  move: function(x, y) {
     this.legalMoves = []
     if (this._canMove(x, y)) {
       var temp = board.grid[this.pos.y][this.pos.x]
@@ -80,7 +82,7 @@ function Pawn(row, col, color) { // can only move forward : 1 case (TO DO: 2 cas
   this.range = 1
 }
 
-function King(row, col, color) { // can only move forward : 1 case (TO DO: 2 case if firstmove)
+function King(row, col, color) {
   this.class = 'king'
   this.color = color
   this.pos = {
@@ -90,8 +92,9 @@ function King(row, col, color) { // can only move forward : 1 case (TO DO: 2 cas
   this.legalMoves = []
   this.range = 1
 }
+King.prototype = Object.create(PiecePrototype)
 
-function Rook(row, col, color) { // can only move forward : 1 case (TO DO: 2 case if firstmove)
+function Rook(row, col, color) {
   this.class = 'rook'
   this.color = color
   this.pos = {
@@ -103,7 +106,7 @@ function Rook(row, col, color) { // can only move forward : 1 case (TO DO: 2 cas
 }
 Rook.prototype = Object.create(PiecePrototype)
 
-function Knight(row, col, color) { // can only move forward : 1 case (TO DO: 2 case if firstmove)
+function Knight(row, col, color) {
   this.class = 'knight'
   this.color = color
   this.pos = {
@@ -115,7 +118,7 @@ function Knight(row, col, color) { // can only move forward : 1 case (TO DO: 2 c
 }
 Knight.prototype = Object.create(PiecePrototype)
 
-function Bishop(row, col, color) { // can only move forward : 1 case (TO DO: 2 case if firstmove)
+function Bishop(row, col, color) {
   this.class = 'knight'
   this.color = color
   this.pos = {
@@ -126,6 +129,18 @@ function Bishop(row, col, color) { // can only move forward : 1 case (TO DO: 2 c
   this.range = 3
 }
 Bishop.prototype = Object.create(PiecePrototype)
+
+function Queen(row, col, color) {
+  this.class = 'queen'
+  this.color = color
+  this.pos = {
+    x: col,
+    y: row,
+  }
+  this.legalMoves = []
+  this.range = 3
+}
+Queen.prototype = Object.create(PiecePrototype)
 
 ///////////////////////////////////////////////
 //                  PAWN                     //
@@ -209,19 +224,6 @@ Pawn.prototype._canAttack = function(x, y) {
 //                  KING                     //
 ///////////////////////////////////////////////
 
-
-King.prototype.move = function(x, y) {
-  this.legalMoves = [] // reset legal moves
-  if (this._canMove(x, y)) {
-    var temp = board.grid[this.pos.y][this.pos.x]
-    board.grid[y][x] = temp
-    board.grid[this.pos.y].splice(this.pos.x, 1, null)
-    this.pos.y = y
-    this.pos.x = x
-    board.render()
-  }
-}
-
 King.prototype._canMove = function(x, y) {
   var that = this
   if (this.color === 'white') {
@@ -230,11 +232,6 @@ King.prototype._canMove = function(x, y) {
         this.legalMoves.push([i, j])
       }
     }
-    this.legalMoves.splice(4, 1)
-    var legal = this.legalMoves.some(function(moveset) {
-      return moveset[0] === x && moveset[1] === y
-    })
-    if (legal) return true
   } else {
     for (i = this.pos.y - 1; i <= this.pos.y + 1; i++) { //scope of legal moves
       for (j = this.pos.x - 1; j <= this.pos.x + 1; j++) {
@@ -242,12 +239,9 @@ King.prototype._canMove = function(x, y) {
       }
     }
     this.legalMoves.splice(4, 1)
-    var legal = this.legalMoves.some(function(moveset) {
-      return moveset[0] === x && moveset[1] === y
-    })
-    if (legal) return true
   }
-  return false
+  console.log(this.legalMoves)
+  return this.checkLegal(x, y)
 }
 
 ///////////////////////////////////////////////
@@ -296,19 +290,7 @@ Rook.prototype._canMove = function(x, y) {
     }
   }
   console.log(this.legalMoves)
-  return this.checkLegal(x,y)
-}
-
-Rook.prototype.move = function(x, y) {
-  this.legalMoves = []
-  if (this._canMove(x, y)) {
-    var temp = board.grid[this.pos.y][this.pos.x]
-    board.grid[y][x] = temp
-    board.grid[this.pos.y].splice(this.pos.x, 1, null)
-    this.pos.y = y
-    this.pos.x = x
-    board.render()
-  }
+  return this.checkLegal(x, y)
 }
 
 ///////////////////////////////////////////////
@@ -317,15 +299,15 @@ Rook.prototype.move = function(x, y) {
 
 Knight.prototype._canMove = function(x, y) {
   var that = this
-  for(i=(this.pos.y-2);i<=(this.pos.y+2);i+=4) {
-    this.legalMoves.push([i,this.pos.x+1])
-    this.legalMoves.push([i,this.pos.x-1])
+  for (i = (this.pos.y - 2); i <= (this.pos.y + 2); i += 4) {
+    this.legalMoves.push([i, this.pos.x + 1])
+    this.legalMoves.push([i, this.pos.x - 1])
   }
-    for(j=(this.pos.x-2);j<=(this.pos.x+2);j+=4) {
-    this.legalMoves.push([this.pos.y+1,j])
-    this.legalMoves.push([this.pos.y-1,j])
+  for (j = (this.pos.x - 2); j <= (this.pos.x + 2); j += 4) {
+    this.legalMoves.push([this.pos.y + 1, j])
+    this.legalMoves.push([this.pos.y - 1, j])
   }
-  var caseExist = this.legalMoves.filter(function (item) {
+  var caseExist = this.legalMoves.filter(function(item) {
     return (item[0] >= 0 && item[1] >= 0) || (item[0] < this.length && item[1] < this.length)
   }).filter(function(item) {
     if (!board.grid[item[0]][item[1]]) return true
@@ -333,7 +315,7 @@ Knight.prototype._canMove = function(x, y) {
   })
   this.legalMoves = caseExist
   console.log(this.legalMoves)
-  return this.checkLegal(x,y)
+  return this.checkLegal(x, y)
 }
 
 ///////////////////////////////////////////////
@@ -398,6 +380,110 @@ Bishop.prototype._canMove = function(x, y) {
   console.log(this.legalMoves)
   return this.checkLegal(x, y)
 }
+
+///////////////////////////////////////////////
+//                  QUEEN                    //
+///////////////////////////////////////////////
+
+Queen.prototype._canMove = function(x, y) {
+  var X = this.pos.x
+  var Y = this.pos.y
+  for (i = 1; i < board.grid.length; i++) { //MOVE UP LEFT
+    if (board.grid[Y - i]) {
+      if (board.grid[Y - i][X - i]) {
+        if (this.color !== board.grid[Y - i][X - i].color) { //MAKE BLOCKER POSITION LEGAL IF ENEMY
+          this.legalMoves.push([X - i, Y - i])
+        }
+        i = board.grid.length // STOP LOOP ON BLOCKER
+      } else {
+        this.legalMoves.push([X - i, Y - i])
+      }
+    }
+  }
+  for (j = 1; j < board.grid.length; j++) { // MOVE DOWN RIGHT
+    if (board.grid[Y + j]) {
+      if (board.grid[Y + j][X + j]) {
+        if (this.color !== board.grid[Y + j][X + j].color) { //MAKE BLOCKER POSITION LEGAL IF ENEMY
+          this.legalMoves.push([X + j, Y + j])
+        }
+        j = board.grid.length // STOP LOOP ON BLOCKER
+      } else {
+        this.legalMoves.push([X + j, Y + j])
+      }
+    }
+  }
+  for (k = 1; k < board.grid.length; k++) { //MOVE DOWN LEFT 
+    if (board.grid[Y + k]) {
+      if (board.grid[Y + k][X - k]) {
+        if (this.color !== board.grid[Y + k][X - k].color) { //MAKE BLOCKER POSITION LEGAL IF ENEMY
+          this.legalMoves.push([X - k, Y + k])
+        }
+        k = board.grid.length // STOP LOOP ON BLOCKER
+      } else {
+        this.legalMoves.push([X - k, Y + k])
+      }
+    }
+  }
+  for (l = 1; l < board.grid.length; l++) { // MOVE UP RIGHT
+    if (board.grid[Y - l]) {
+      if (board.grid[Y - l][X + 1]) {
+        if (this.color !== board.grid[Y - l][X + l].color) { //MAKE BLOCKER POSITION LEGAL IF ENEMY
+          this.legalMoves.push([X + l, Y - l])
+        }
+        l = board.grid.length // STOP LOOP ON BLOCKER
+      } else {
+        this.legalMoves.push([X + l, Y - l])
+      }
+    }
+  }
+  for (i = this.pos.y + 1; i < board.grid.length; i++) { //MOVE DOWN
+    if (board.grid[i][this.pos.x]) {
+      if (this.color !== board.grid[i][this.pos.x].color) { //MAKE BLOCKER POSITION LEGAL IF ENEMY
+        this.legalMoves.push([this.pos.x, i])
+      }
+      i = board.grid.length // STOP LOOP ON BLOCKER
+    } else {
+      this.legalMoves.push([this.pos.x, i])
+    }
+  }
+  for (j = this.pos.y - 1; j > 0; j--) { //MOVE UP
+    if (board.grid[j][this.pos.x]) {
+      if (this.color !== board.grid[j][this.pos.x].color) { //MAKE BLOCKER POSITION LEGAL IF ENEMY
+        this.legalMoves.push([this.pos.x, j])
+      }
+      j = 0 // STOP LOOP ON BLOCKER
+    } else {
+      this.legalMoves.push([this.pos.x, j])
+    }
+  }
+  for (k = this.pos.x + 1; k < board.grid.length; k++) { //MOVE RIGHT
+    if (board.grid[this.pos.y][k]) {
+      if (this.color !== board.grid[this.pos.y][k].color) { //MAKE BLOCKER POSITION LEGAL IF ENEMY
+        this.legalMoves.push([k, this.pos.y])
+      }
+      k = board.grid.length // STOP LOOP ON BLOCKER
+    } else {
+      this.legalMoves.push([k, this.pos.y])
+    }
+  }
+  for (l = this.pos.x - 1; l > 0; l--) { //MOVE LEFT
+    if (board.grid[this.pos.y][l]) {
+      if (this.color !== board.grid[this.pos.y][l].color) { //MAKE BLOCKER POSITION LEGAL IF ENEMY
+        this.legalMoves.push([l, this.pos.y])
+      }
+      l = 0 // STOP LOOP ON BLOCKER
+    } else {
+      this.legalMoves.push([l, this.pos.y])
+    }
+  }
+  var caseExist = this.legalMoves.filter(function(item) {
+    return (item[0] >= 0 && item[1] >= 0) && (item[0] < board.grid.length && item[1] < board.grid.length) // UGLY TRICK TO REMOVE OUT OF BOUNDS MOVES
+  })
+  this.legalMoves = caseExist
+  console.log(this.legalMoves)
+  return this.checkLegal(x, y)
+}
+
 
 var board = new Board()
 
